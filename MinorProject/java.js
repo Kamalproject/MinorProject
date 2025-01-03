703286b552605db7a8a6cc0b4f9feabaf9568c5b
Kamallochan Panigrahi <143154588+Kamalproject@users.noreply.github.com>
2025-01-03 22:44:52 +0530

    Add files via upload

---
+++ MinorProject/java.js
@@ -0,0 +1,28 @@
+var time=new Date().getHours();
+if(time<12){
+    alert("Good Morning");
+}
+else if(time==12){
+    alert("Good Noon");
+}
+else if(time>12 && time<20){
+    alert("Good Evening");
+}
+else{
+    alert("Good Night");
+}
+
+
+
+
+function resetContent(){
+    document.getElementById("search").value="";
+}
+
+function searchContent(){
+    // Get the input value
+    var input = document.getElementById("search").value;
+  
+    // Display the input value on the screen
+    document.getElementById("searchResult").innerHTML = "You searched for: " + input;
+  }
